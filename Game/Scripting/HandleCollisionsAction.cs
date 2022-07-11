1495ac4d0d220aa38444d8ab4ad609e235d1b724
Broncelino <bsojn27@gmail.com>
2022-07-11 13:23:44 -0600

    added end function only runs while currently touching enemy

--- Game/Scripting/HandleCollisionsAction.cs
+++ Game/Scripting/HandleCollisionsAction.cs
@@ -11,16 +11,25 @@ namespace Scripting
         {
             
         }
-
+        
 
         public override void Execute(Scene scene, float deltaTime, IActionCallback callback)
         {
             Actor player = scene.GetFirstActor("player");
             List<Casting.Actor> enemies = scene.GetAllActors("enemies");
+
+            //function to end the game, probably give its own file.
+            void end(){
+            player.Tint(Color.White());
+            foreach (Actor enemy in enemies){
+                enemy.Tint(Color.White());
+            }
+            }
+            
             foreach (Actor enemy in enemies){
                if(player.Overlaps(enemy))
                {
-                player.Tint(Color.White());                   
+                end();             
                     //Code to end game!
                }
             }
