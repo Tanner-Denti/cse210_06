7dc0a87091f687de4a30893ea96652df8fd95d44
Drew LaMunyon <galpha7@gmail.com>
2022-07-13 12:46:19 -0600

    Started Work on Bullet Collision

---
+++ Game/Scripting/HandleWeaponCollisionsAction.cs
@@ -0,0 +1,29 @@
+// using System;
+// using System.Collections.Generic;
+// using Casting;
+// using Services;
+
+// namespace Scripting 
+// {
+//     public class HandleWeaponCollisionsAction : Scripting.Action 
+//     {
+//         public HandleWeaponCollisionsAction()
+//         {
+            
+//         }
+        
+
+//         public override void Execute(Scene scene, float deltaTime, IActionCallback callback)
+//         {
+//             Actor bullet = scene.GetFirstActor("player");
+//             List<Actor> bullets = scene.GetAllActors("bullets");
+//             List<Actor> enemies = scene.GetAllActors("enemies");
+//             foreach(Actor bullet in bullets)
+//             {
+                
+//             }
+
+//         }
+            
+//     }
+// }
